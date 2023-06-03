a95a3e5547126e65ad00d7bbb89fdbcff3e4219f
Afril <afril@youdingapp.com>
2023-06-04 00:52:29 +0700

    Add mp3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
-import { defineComponent } from "vue"
-import MovingCloud from "./components/MovingCloud.vue"
-import WarScene from "./components/WarScene.vue"
+import { defineComponent } from "vue";
+import MovingCloud from "./components/MovingCloud.vue";
+import WarScene from "./components/WarScene.vue";
 
 export default defineComponent({
   components: {
@@ -11,22 +11,24 @@ export default defineComponent({
   data() {
     return {
       warScene: false as boolean,
-    }
+    };
   },
   methods: {
     play() {
-      const audio = this.$refs.khmerRepublic as any
-      this.warScene = !this.warScene
-      this.warScene ? audio.play() : (audio.pause(), (audio.currentTime = 0))
+      const audio = this.$refs.khmerRepublic as any;
+      this.warScene = !this.warScene;
+      this.warScene ? audio.play() : (audio.pause(), (audio.currentTime = 0));
     },
   },
   mounted() {},
-})
+});
 </script>
 
 <template>
-  <audio ref="khmerRepublic" src="/src/assets/audio/completed.mp3" autoplay></audio>
   <div class="">
+    <audio hidden="true" ref="khmerRepublic" autoplay>
+      <source src="./assets/audio/completed.mp3" type="audio/mpeg" />
+    </audio>
     <div
       ref="container"
       class="my-auto mx-auto w-[927px] h-[700px] border-[#d19d5c] border-[12px] grid place-content-center"
