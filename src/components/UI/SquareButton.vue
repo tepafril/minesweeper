ab5e4db03d79aa8a6787b7d2743e2aba2415cc09
Afril <afril@youdingapp.com>
2023-06-10 12:53:03 +0700

    Deploy

--- src/components/UI/SquareButton.vue
+++ src/components/UI/SquareButton.vue
@@ -71,9 +71,9 @@ export default defineComponent({
   background-image: url("./sprites/ui/music-button.png");
 }
 .gmail-button {
-  background-image: url("./sprites/ui/gmail-button.png");
+  background-image: url("~@/sprites/ui/gmail-button.png");
 }
 .linkedin-button {
-  background-image: url("./sprites/ui/linkedin-button.png");
+  background-image: url("@/sprites/ui/linkedin-button.png");
 }
 </style>
